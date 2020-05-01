65613892dfc4f9f4ff9448855e9aad6d7b96c57e
Xiang Song <himmelsong@gmail.com>
2020-04-30 19:05:13 -0700

    API key stored

---
+++ script.js
@@ -0,0 +1,16 @@
+//API key: 7IYuck6YDV4vC4epazHfoWbj42W4vbUw
+const Akey = "7IYuck6YDV4vC4epazHfoWbj42W4vbUw";
+
+
+let displayResults = () => {
+
+};
+
+let fetchDate = () => {
+
+};
+
+let userRequest = () => {
+    let criteria = "";
+
+};
