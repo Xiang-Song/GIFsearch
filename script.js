60ffb1ef647d35956f06b5e4f797ef548582be4f
Xiang Song <himmelsong@gmail.com>
2020-05-01 09:41:59 -0700

    finish js

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
+
 const Akey = "odBYLKqVyDIUumIT4qC1USPdko451Pzk";
-var criteria = "giphy";
-var maxNumber = "20";
-var rating = "G";
+let criteria = "giphy";
+let maxNumber = 20;
+let rating = "g";
 
 
-let displayResults = () => {
-
+            
+        
+let displayResults = (d) => {
+    let content = "";
+    $('#results').empty();
+    for (let arr of d) {
+        content = `<div class="col-sm-3"><img src=${arr.images.original.url}></div>`;
+        $('#results').append(content);
+    }; 
 };
 
-let fetchDate = () => {
-    fetch('')
+let fetchDate = (criteria, maxNumber, rating) => {
+    let URL = `https://api.giphy.com/v1/gifs/search?api_key=${Akey}&q=${criteria}&limit=${maxNumber}&rating=${rating}`
+    fetch(URL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (result) {
+            displayResults(result.data)
+        })
 };
 
 let userRequest = () => {
-    $("#submit").on('click', ()=> {
-        var criteria = $("#criteria").val();
-        var maxNumber = $("#max-number").val();
-        var rating = $("rating").val();
+    $("#submit").on('click', () => {
+        let criteria = $("#criteria").val()?$("#criteria").val():'gifhy';
+        $("#criteria").val('');
+        let maxNumber = $("#max-number").val()?$("#max-number").val():20;
+        $("#max-number").val('');
+        let rating = $("#rating").find(":selected").val();
+        fetchDate(criteria, maxNumber, rating);
     });
-    $("#entertainment").on('click', ()=> {
-        var criteria = "entertainment"
+    $("#entertainment").on('click', function () {
+        let criteria = "entertainment";
+        fetchDate(criteria, maxNumber, rating);
     });
-    $("#Sports").on('click', ()=> {
-        var criteria = "sports"
+    $("#Sports").on('click', () => {
+        let criteria = "sports";
+        fetchDate(criteria, maxNumber, rating);
     });
-    $("#Animals").on('click', ()=> {
-        var criteria = "animal"
+    $("#Animals").on('click', () => {
+        let criteria = "animal";
+        fetchDate(criteria, maxNumber, rating);
     });
-    $("#Emotions").on('click', ()=> {
-        var criteria = "emotion"
+    $("#Emotions").on('click', () => {
+        let criteria = "emotion";
+        fetchDate(criteria, maxNumber, rating);
     });
+    fetchDate(criteria, maxNumber, rating);
 };
+
+
+$(userRequest);
+
